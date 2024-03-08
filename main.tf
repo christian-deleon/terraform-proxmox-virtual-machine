47f9444191ee01a018a83538053fe0a0dd2d383b
Christian De Leon <christiandeleon12693@gmail.com>
2024-03-08 12:31:58 -0500

    Refactor main.tf and variables.tf files

--- main.tf
+++ main.tf
@@ -7,26 +7,45 @@ terraform {
 }
 
 resource "proxmox_vm_qemu" "this" {
+  # General settings
   name        = var.name
   vmid        = var.vmid
   desc        = var.description
   target_node = var.target_node
-  agent       = var.qemu_agent ? "1" : "0"
-  hastate     = var.hastate
-  hagroup     = var.hagroup
   clone       = var.template_name
-  os_type     = var.os_type
+  agent       = var.qemu_agent ? "1" : "0"
 
-  # Hardware
-  cores   = var.cores
-  sockets = var.sockets
-  memory  = var.memory
+  # Cloud-init settings
+  os_type                 = var.os_type
+  ciuser                  = var.cloudinit_user
+  cipassword              = var.cloudinit_password
+  sshkeys                 = var.cloudinit_sshkey
+  cloudinit_cdrom_storage = var.cloudinit_cdrom
+
+  # Hardware settings
+  sockets  = var.sockets
+  cores    = var.cores
+  numa     = var.numa
+  vcpus    = var.vcpus
+  cpu      = var.cpu
+  memory   = var.memory
+  scsihw   = var.scsihw
+  bootdisk = var.bootdisk
+
+  # High Availability settings
+  hastate = var.hastate
+  hagroup = var.hagroup
+
+  # Network settings
+  ipconfig0  = var.ipconfig0_ip == null ? null : "ip=${var.ipconfig0_ip}/${var.ipconfig0_cidr},gw=${var.ipconfig0_gw}"
+  nameserver = var.nameserver
 
   network {
     model  = var.network_model
     bridge = var.network_bridge
   }
 
+  # Disk settings
   disks {
     scsi {
       scsi0 {
--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 ############################################
-# Proxmox
+# General settings
 ############################################
 
 variable "name" {
@@ -29,32 +29,60 @@ variable "template_name" {
   description = "The name of the template to clone"
 }
 
-variable "os_type" {
-  type        = string
-  description = "The type of the operating system. Options: ubuntu, centos, cloud-init"
-}
-
 variable "qemu_agent" {
   type        = bool
   default     = true
   description = "Enable the QEMU Guest Agent"
 }
 
-variable "hastate" {
+############################################
+# Cloud-init settings
+############################################
+
+variable "os_type" {
   type        = string
-  default     = null
-  description = "The HA state of the virtual machine. One of 'started', 'stopped', 'enabled', 'disabled', or 'ignored'"
+  description = "The type of the operating system. Options: ubuntu, centos, cloud-init"
 }
 
-variable "hagroup" {
+variable "cloudinit_user" {
   type        = string
   default     = null
-  description = "The HA group of the virtual machine. HA State must be set."
+  description = "The cloud-init user"
+}
+
+variable "cloudinit_password" {
+  type        = string
+  default     = null
+  description = "The cloud-init user password"
+}
+
+variable "cloudinit_sshkey" {
+  type        = string
+  default     = null
+  description = "The cloud-init user ssh key"
+}
+
+variable "cloudinit_cdrom" {
+  type        = string
+  default     = null
+  description = "The cloud-init cdrom storage name"
+}
+
+variable "cloudinit_custom" {
+  type        = string
+  default     = null
+  description = "The cloud-init custom configuration"
 }
 
 ############################################
-# Hardware
+# Hardware settings
 ############################################
+
+variable "sockets" {
+  type        = number
+  default     = 1
+  description = "The number of sockets"
+}
 
 variable "cores" {
   type        = number
@@ -62,10 +90,22 @@ variable "cores" {
   description = "The number of cores"
 }
 
-variable "sockets" {
-  type        = number
-  default     = 1
-  description = "The number of sockets"
+variable "vcpus" {
+  type        = string
+  default     = "0"
+  description = "The number of vcpus"
+}
+
+variable "cpu" {
+  type        = string
+  default     = "host"
+  description = "The CPU type. Options: host, kvm64, kvm32, core2duo, pentium, phenom, qemu32, qemu64"
+}
+
+variable "numa" {
+  type        = bool
+  default     = false
+  description = "Enable NUMA"
 }
 
 variable "memory" {
@@ -85,6 +125,22 @@ variable "storage_name" {
   description = "The name of the storage"
 }
 
+variable "scsihw" {
+  type        = string
+  default     = null
+  description = "The SCSI controller type. Options: lsi, virtio-scsi-pci, virtio-scsi-single"
+}
+
+variable "bootdisk" {
+  type        = string
+  default     = "scsi0"
+  description = "The boot disk"
+}
+
+############################################
+# Network settings
+############################################
+
 variable "network_model" {
   type        = string
   default     = "virtio"
@@ -95,4 +151,40 @@ variable "network_bridge" {
   type        = string
   default     = "vmbr0"
   description = "The network bridge"
+}
+
+variable "ipconfig0_ip" {
+  type    = string
+  default = null
+}
+
+variable "ipconfig0_cidr" {
+  type    = string
+  default = null
+}
+
+variable "ipconfig0_gw" {
+  type    = string
+  default = null
+}
+
+variable "nameserver" {
+  type    = string
+  default = null
+}
+
+############################################
+# High Availability (HA) Settings
+############################################
+
+variable "hastate" {
+  type        = string
+  default     = null
+  description = "The HA state of the virtual machine. One of 'started', 'stopped', 'enabled', 'disabled', or 'ignored'"
+}
+
+variable "hagroup" {
+  type        = string
+  default     = null
+  description = "The HA group of the virtual machine. HA State must be set."
 }
